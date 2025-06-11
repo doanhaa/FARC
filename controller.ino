001277ecaa12f8bba6db3b6f10270f76f70c06ea
HaThai <99739006+doanhaa@users.noreply.github.com>
2025-06-11 17:58:06 +0700

    delete comment

--- controller.ino
+++ controller.ino
@@ -1,12 +1,3 @@
-/*
- * A better version of MakerBotwPS2 example used with makerbot BANHMI hardware platform
- * This version simplify the joystick driving method, where the functionality stay the same.
- * Added new controling menthod, switch driving mode by pressing SELECT button
- * Added nitro like speed booster when holding L2
- * 
- * Writen by Tu Dang - Makerviet
- */
-
 #include "motors.h"
 #include "PS2_controller.h"
 
